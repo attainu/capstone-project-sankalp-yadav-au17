95ed0c6738502d6f0f271ebd7bbdc53f7666dd54
Abhishek <abhishekc838@gmail.com>
2022-01-25 18:33:44 +0530

    NetFlix File

---
+++ netflix/src/components/navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import {Search, Notifications, ArrowDropDown} from "@material-ui/icons"
+import {useState} from 'react'
+import "./navbar.scss"
+
+function Navbar() {
+    const [isScrolled, setIsScrolled] = useState(false)
+
+   window.onscroll = () => {
+       setIsScrolled(window)
+   }
+    return (
+        <div className="navbar">
+            <div className="container">
+                <div className="left">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png
+" alt="logo" />
+                <span>Homepage</span>
+                <span>Series</span>
+                <span>Movies</span>
+                <span>New And Popular</span>
+                <span>My List</span>
+                </div>
+                <div className="right">
+                    <Search className="icon"/>
+                    <span>KIDS</span>
+                    <Notifications className="icon"/>
+                    <img src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="avtar" />
+
+                    <div className="profile">
+                        <ArrowDropDown className="icon"/>
+
+                        <div className="options">
+                            <span>Settings</span>
+                            <span>Logout</span>
+                        </div>
+                    </div>
+                    
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export { Navbar }
