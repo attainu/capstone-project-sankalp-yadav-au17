9381067397b289be79718924a91bf2f45aa47c1c
sankalp-yadav-au17 <sankalp_ma@srmuniv.edu.in>
2022-01-27 21:59:38 +0530

    design complete

---
+++ netflix/client/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.scss';
+import Home from './pages/home/Home';
+import Register from './pages/register/Register';
+import Watch from './pages/watch/watch';
+import Login from './pages/login/login';
+
+function App() {
+  return (
+    <div>
+        <Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ netflix/client/src/pages/home/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import Featured from '../../components/Featured/Featured'
+import "./Home.scss"
+import {Navbar} from "../../components/navbar/Navbar"
+import List from "../../components/list/list"
+
+
+function Home() {
+    return (
+        <div className="home">
+           <Navbar/>
+           <Featured/>
+           <List/>
+           <List/>
+           <List/>
+           <List/>
+           <List/>
+        </div>
+    )
+}
+
+export default Home
